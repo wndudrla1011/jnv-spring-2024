8edfd082b5e7751b610bad96906690db08e81ae6
wndudrla1011 <wndudrla1011@naver.com>
2024-08-29 17:32:17 +0900

    practice applying TransactionTemplate

---
+++ TransactionTemplate/src/main/java/com/javalec/ex/dao/TicketDao.java
@@ -0,0 +1,71 @@
+package com.javalec.ex.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import com.javalec.ex.dto.TicketDto;
+
+public class TicketDao {
+
+	JdbcTemplate template;
+
+	TransactionTemplate transactionTemplate;
+
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
+	}
+
+	public TicketDao() {
+		System.out.println(template);
+	}
+
+	public void buyTicket(final TicketDto dto) {
+		System.out.println("buyTicket()");
+		System.out.println("dto.getConsumerId() : " + dto.getConsumerId());
+		System.out.println("dto.getAmount() : " + dto.getAmount());
+
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				template.update(new PreparedStatementCreator() {
+
+					@Override
+					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+						String query = "insert into card (consumerId, amount) values (?, ?)";
+						PreparedStatement pstmt = con.prepareStatement(query);
+						pstmt.setString(1, dto.getConsumerId());
+						pstmt.setString(2, dto.getAmount());
+
+						return pstmt;
+					}
+				});
+
+				template.update(new PreparedStatementCreator() {
+
+					@Override
+					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+						String query = "insert into ticket (consumerId, countnum) values (?, ?)";
+						PreparedStatement pstmt = con.prepareStatement(query);
+						pstmt.setString(1, dto.getConsumerId());
+						pstmt.setString(2, dto.getAmount());
+
+						return pstmt;
+					}
+				});
+			}
+		});
+	}
+
+}

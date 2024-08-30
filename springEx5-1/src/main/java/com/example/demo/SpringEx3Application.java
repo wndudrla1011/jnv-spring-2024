9df0344bd61a2ef368f9ca0b6afdb01d83ab957d
wndudrla1011 <wndudrla1011@naver.com>
2024-08-30 15:50:50 +0900

    added spring ex 5-1

---
+++ springEx5-1/src/main/java/com/example/demo/SpringEx3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEx3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEx3Application.class, args);
+	}
+
+}

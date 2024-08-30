95ffb5f403e710a41f3fb8ffa8bc0add9587e679
wndudrla1011 <wndudrla1011@naver.com>
2024-08-30 15:52:35 +0900

    added spring ex 6-1

---
+++ springEx6-1/src/main/java/com/example/demo/StudentInfo.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class StudentInfo {
+
+	private Student student;
+
+	public StudentInfo() {
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+}

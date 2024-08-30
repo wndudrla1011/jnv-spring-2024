b1cecf4ca4a2be43de2d1edfbd8ed3632e44358b
wndudrla1011 <wndudrla1011@naver.com>
2024-08-30 15:59:09 +0900

    added spring ex 11-1

---
+++ springEx11-1/src/main/java/com/example/demo/SpringEx112Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEx112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEx112Application.class, args);
+	}
+
+}

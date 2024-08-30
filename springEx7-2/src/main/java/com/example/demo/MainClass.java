4b7a0b25f8d82af59970fb8dbbe3e242e99e9189
wndudrla1011 <wndudrla1011@naver.com>
2024-08-30 15:55:45 +0900

    added spring ex 7-2

---
+++ springEx7-2/src/main/java/com/example/demo/MainClass.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+
+		ctx.load("classpath:applicationCTX.xml");
+
+		ctx.refresh();
+
+		ctx.close();
+
+	}
+
+}
---
+++ springEx7-2/src/main/java/com/example/demo/SpringEx72Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEx72Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEx72Application.class, args);
+	}
+
+}
